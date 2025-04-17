c38bf8ae88cbccd6a03dee6a6e040838ed4a7435
Ridham151101 <ridham.patel@bacancy.com>
2025-04-18 01:06:39 +0530

    Refactor tests to use subject and reduce duplication

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,24 +1,23 @@
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+  subject(:calculator) { described_class.new }
+
   describe '#add' do
     context 'with empty string' do
       it 'returns 0' do
-        calculator = StringCalculator.new
         expect(calculator.add('')).to eq(0)
       end
     end
 
     context 'with a single number' do
       it 'returns the number itself' do
-        calculator = StringCalculator.new
         expect(calculator.add('1')).to eq(1)
       end
     end
 
     context 'with two comma-separated numbers' do
       it 'returns the sum of the numbers' do
-        calculator = StringCalculator.new
         expect(calculator.add('1,5')).to eq(6)
       end
     end
